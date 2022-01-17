dc6ae22821c202bab4e35924733547525ea266c7
MarshalTito7 <parijat.niyogy7@gmail.com>
2022-01-17 21:53:53 +0530

    second commit

---
+++ app.js
@@ -0,0 +1,19 @@
+
+// Init weather object
+const weather = new Weather('London','uk');
+// Init UI object
+// const ui = new UI();
+
+// Get weather on DOM load
+document.addEventListener('DOMContentLoaded',getWeather);
+
+// weather.changeLocation
+
+function getWeather(){
+    weather.getWeather()
+    .then(results => {
+        // ui.paint(results);
+        console.log(results);
+    })
+    .catch(err => console.log(err));
+}
--- weather.js
+++ weather.js
@@ -8,13 +8,17 @@ class Weather{
 
     // Fetch weather from api
     async getWeather(){
-        const response = await fetch(`api.openweathermap.org/data/2.5/weather?q=${this.city},${this.state}&appid=${this.apiKey}`);
+        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.state}&appid=${this.apiKey}`);
 
         const responseData = await response.json();
 
         return responseData;
     }
 
-    
+    // change weather location
+    changeLocation(city, state){
+        this.city = city;
+        this.state = state;
+    }
 
 }
