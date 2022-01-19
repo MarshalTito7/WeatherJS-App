39ff13fc4badd96e748982feb5b1b96247fbfaf1
MarshalTito7 <parijat.niyogy7@gmail.com>
2022-01-19 22:25:42 +0530

    fourth commit

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+// Init Storage
+const storage = new Storage();
+// Get stored location data
+const weatherLocation = storage.getLocationData();
 
 // Init weather object
-const weather = new Weather('Boston','US');
+const weather = new Weather(weatherLocation.city, weatherLocation.country);
 // Init UI object
 const ui = new UI();
 
@@ -12,8 +16,11 @@ document.getElementById('w-change-btn').addEventListener('click',(e) => {
     const city = document.getElementById('city').value;
     const country = document.getElementById('country').value;
 
-    // console.log(city,country);
+    //Change location
     weather.changeLocation(city,country);
+
+    // Set location in LS
+    storage.setLocationData(city,country);
 
     // Get and display weather
     getWeather();
